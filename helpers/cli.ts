50f167602fdb27af38fc4866930ec566ee91f7a0
linkcube <poohbah125@gmail.com>
2020-01-05 14:53:13 -0800

    helpers

---
+++ helpers/cli.ts
@@ -0,0 +1,43 @@
+const commandLineUsage = require('command-line-usage')
+
+const sections = [
+    {
+        header: "Nora",
+        content: "Node R/a/dio archiver, for saving your streams. For a gui check out: {underline https://github.com/Linkcube/svelte-radio-interface}"
+    },
+    {
+        header: "Options",
+        optionList: [
+            {
+                name: "load_config",
+                alias: "l",
+                typeLabel: "{underline file}",
+                description: "Loads a JSON config file for the main program, use with --start."
+            },
+            {
+                name: "start",
+                alias: "s",
+                type: Boolean,
+                description: "Starts the main program."
+            },
+            {
+                name: "process",
+                alias: "p",
+                typeLabel: "{underline file}",
+                description: "Process a previous recording."
+            }
+        ]
+    },
+    {
+        content: 'Project home: {underline https://github.com/Linkcube/Nora}'
+      }
+]
+
+export const usage = commandLineUsage(sections);
+
+export const cli_opts = [
+    { name: "load_config", alias: "l", type: String },
+    { name: "start", type: Boolean },
+    { name: "process", alias: "p", type: String },
+    { name: "help", alias: "h", type: Boolean }
+];
